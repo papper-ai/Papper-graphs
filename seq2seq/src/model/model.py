ebe1b0b9c6631bd02d13880bc311739735bbd255
Artyom Eryomkin <43377589+RakePants@users.noreply.github.com>
2024-03-01 22:54:57 +0500

    Change model name and add to gitignore

--- seq2seq/src/model/model.py
+++ seq2seq/src/model/model.py
@@ -5,8 +5,8 @@ from src.config import settings
 
 CUDA_IS_AVAILABLE = torch.cuda.is_available()
 
-tokenizer = AutoTokenizer.from_pretrained("src/model/rut5-REBEL-base") if settings.lm_mode == 'local' else AutoTokenizer.from_pretrained("memyprokotow/rut5-REBEL-base")
-model = AutoModelForSeq2SeqLM.from_pretrained("src/model/rut5-REBEL-base") if settings.lm_mode == 'local' else AutoTokenizer.from_pretrained("memyprokotow/rut5-REBEL-base")
+tokenizer = AutoTokenizer.from_pretrained("src/model/rut5_REBEL_base") if settings.lm_mode == 'local' else AutoTokenizer.from_pretrained("memyprokotow/rut5-REBEL-base")
+model = AutoModelForSeq2SeqLM.from_pretrained("src/model/rut5_REBEL_base") if settings.lm_mode == 'local' else AutoTokenizer.from_pretrained("memyprokotow/rut5-REBEL-base")
 
 if CUDA_IS_AVAILABLE:
     model.to("cuda")
