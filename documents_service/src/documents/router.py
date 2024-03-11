2631866ab5de0294e3d8bc09f3c3f6855ef014f9
Artyom Eryomkin <43377589+RakePants@users.noreply.github.com>
2024-03-11 21:24:22 +0500

    Add create vault input schema

--- documents_service/src/documents/router.py
+++ documents_service/src/documents/router.py
@@ -1,3 +1,17 @@
-from fastapi import APIRouter
+from typing import List
+
+from fastapi import APIRouter, Body, File, UploadFile
+
+from src.documents.schemas import CreateVaultRequest
 
 documents_router = APIRouter(tags=["Documents"])
+
+
+@documents_router.post("/create_vault")
+async def create_vault(
+    create_vault_request: CreateVaultRequest = Body(...),
+    files: List[UploadFile] = File(...),
+):
+    
+    print("Files received: ", [f.filename for f in files])
+    return {"message": "Files received"}
---
+++ documents_service/src/documents/schemas.py
@@ -0,0 +1,23 @@
+import uuid
+from enum import Enum
+
+import json
+from pydantic import BaseModel, model_validator
+
+
+class VaultType(str, Enum):
+    GRAPH = "graph"
+    VECTOR = "vector"
+
+
+class CreateVaultRequest(BaseModel):
+    uuid: uuid.UUID
+    vault_name: str
+    vault_type: VaultType
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_to_json(cls, value):
+        if isinstance(value, str):
+            return cls(**json.loads(value))
+        return value
