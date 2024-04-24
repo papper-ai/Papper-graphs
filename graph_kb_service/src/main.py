6c041e4ffcb2c4d3fe501c8c87f8f016eb3b3ac6
Artyom Eryomkin <43377589+RakePants@users.noreply.github.com>
2024-04-24 21:00:14 +0500

    Update main.py

--- graph_kb_service/src/main.py
+++ graph_kb_service/src/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.kb_router.router import kb_router
 
 app = FastAPI()
 
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello from graph_kb_service"}
-
 
 app.include_router(kb_router)
