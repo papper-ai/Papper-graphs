631dde0c35fe2c3c1c48dfb3a877e1a13eabd2ab
PixelPantz <kuznec200456@gmail.com>
2024-05-03 01:50:08 +0500

    Generate answer

--- vector_rag_service/src/qa_router/utils.py
+++ vector_rag_service/src/qa_router/utils.py
@@ -4,6 +4,8 @@ from qa_router.schemas import Input
 from config import settings
 from database.queries import search_relevant_chunks
 
+from qa_router.schemas import Answer
+
 
 def create_qa_prompt(context: str, question: str) -> str:
     return f'''
@@ -18,11 +20,17 @@ def create_qa_prompt(context: str, question: str) -> str:
 
 async def generate_answer(input: Input):
     embedding = await create_embedding(input.query)
-    print(f"{embedding=}")
-    relevant_chunks = await search_relevant_chunks(vault_id=input.vault_id, vector=embedding, top_k=2)
-    print(f"{relevant_chunks=}")
+    result = await search_relevant_chunks(vault_id=input.vault_id, vector=embedding, top_k=2)
+    qa_prompt = create_qa_prompt(context=result.text, question=input.query)
+    answer = await create_completion(qa_prompt)
+    return Answer(content=answer, traceback=result.search_result)
+
 
 
 async def create_embedding(query: str) -> List[float]:
     response = await settings.openai_client.embeddings.create(input=[query], model=" ")
     return list(map(lambda x: x.embedding, response.data))[0]
+
+async def create_completion(prompt: str) -> str:
+    response = await settings.openai_client.completions.create(model=" ", prompt=prompt)
+    return response.data[0].choices[0].text
