d1bcc9aaa824e84baf3fdca0f2fe7695979ed18e
Artyom Eryomkin <43377589+RakePants@users.noreply.github.com>
2024-06-05 18:38:12 +0500

    Implement returning final answer on tool use errors

--- graph_rag_service/src/langchain_utils/prompts.py
+++ graph_rag_service/src/langchain_utils/prompts.py
@@ -1,5 +1,13 @@
-from langchain import hub
+import typing
+
+import langchain_core
 from langchain.prompts.prompt import PromptTemplate
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+)
 
 CYPHER_GENERATION_TEMPLATE = """Твоя задача - найти в схеме графа вершинs, чтобы получить о них информацию для ответа на вопрос. Ты должен основываться на предоставленной схеме графа и следовать инструкциям ниже:
 1. Напиши имена вершин в формате списка, например ["вершина1", "вершина2"] без каких-либо других символов;
@@ -75,10 +83,42 @@ Action:
 
 Начинай! Помни, что ВСЕГДА нужно отвечать действительным json-объектом для одного действия. Не используй форматированный вывод. Используй инструменты при необходимости. Отвечай мне напрямую, если это уместно. Формат - Action:```$JSON_BLOB```затем Observation\n\n"""
 
-# Change pre-made prompt template to Russian
-(agent_prompt := hub.pull("hwchase17/structured-chat-agent")).messages[
-    0
-].prompt.template = AGENT_PROMPT_TEMPLATE
-
+(
+    agent_prompt := ChatPromptTemplate(
+        input_variables=["agent_scratchpad", "input", "tool_names", "tools"],
+        input_types={
+            "chat_history": typing.List[
+                typing.Union[
+                    langchain_core.messages.ai.AIMessage,
+                    langchain_core.messages.human.HumanMessage,
+                    langchain_core.messages.chat.ChatMessage,
+                    langchain_core.messages.system.SystemMessage,
+                    langchain_core.messages.function.FunctionMessage,
+                    langchain_core.messages.tool.ToolMessage,
+                ]
+            ]
+        },
+        metadata={
+            "lc_hub_owner": "hwchase17",
+            "lc_hub_repo": "structured-chat-agent",
+            "lc_hub_commit_hash": "ea510f70a5872eb0f41a4e3b7bb004d5711dc127adee08329c664c6c8be5f13c",
+        },
+        messages=[
+            SystemMessagePromptTemplate(
+                prompt=PromptTemplate(
+                    input_variables=["tool_names", "tools"],
+                    template='Respond to the human as helpfully and accurately as possible. You have access to the following tools:\n\n{tools}\n\nUse a json blob to specify a tool by providing an action key (tool name) and an action_input key (tool input).\n\nValid "action" values: "Final Answer" or {tool_names}\n\nProvide only ONE action per $JSON_BLOB, as shown:\n\n```\n{{\n  "action": $TOOL_NAME,\n  "action_input": $INPUT\n}}\n```\n\nFollow this format:\n\nQuestion: input question to answer\nThought: consider previous and subsequent steps\nAction:\n```\n$JSON_BLOB\n```\nObservation: action result\n... (repeat Thought/Action/Observation N times)\nThought: I know what to respond\nAction:\n```\n{{\n  "action": "Final Answer",\n  "action_input": "Final response to human"\n}}\n\nBegin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary. Respond directly if appropriate. Format is Action:```$JSON_BLOB```then Observation',
+                )
+            ),
+            MessagesPlaceholder(variable_name="chat_history", optional=True),
+            HumanMessagePromptTemplate(
+                prompt=PromptTemplate(
+                    input_variables=["agent_scratchpad", "input"],
+                    template="{input}\n\n{agent_scratchpad}\n (reminder to respond in a JSON blob no matter what)",
+                )
+            ),
+        ],
+    )
+).messages[0].prompt.template = AGENT_PROMPT_TEMPLATE
 
 SYSTEM_PROMPT = """Ты - интеллектуальный помощник по имени Papper, который отвечает на вопросы по документам пользователя."""
--- graph_rag_service/src/qa_router/utils.py
+++ graph_rag_service/src/qa_router/utils.py
@@ -23,15 +23,23 @@ async def generate_answer(input: Input) -> Answer:
 
     logging.info(response)
 
-    answer = (
-            response["output"]["result"]
-            if isinstance(response["output"], dict)
-            else response["output"]
-        )
+    # Return the final answer if no intermediate steps were taken
+    if not isinstance(response["output"], dict):
+        return Answer(content=response["output"], traceback=[])
     
-    traceback = []
-    if input.vault_id:
-        if response["intermediate_steps"]:
+    # Return immediate response if tool input is broken
+    if (
+        response["intermediate_steps"][0][0].tool_input
+        == "Invalid or incomplete response"
+    ):
+        return Answer(content=response["output"], traceback=[])
+
+    # Parse taken steps from tool use into a traceback
+    if isinstance(response["output"], dict):
+        answer = response["output"]["result"]
+        
+        traceback = []
+        if input.vault_id:
             for i in range(len(response["intermediate_steps"][0][1]["intermediate_steps"])):
                 try:
                     traceback.extend(
@@ -41,8 +49,8 @@ async def generate_answer(input: Input) -> Answer:
                     )
                 except Exception as e:
                     logging.error(e)
-                    
+
             # Deduplicate traceback
             traceback = [dict(d) for d in {frozenset(d.items()) for d in traceback}]
-            
-    return Answer(content=answer, traceback=traceback)
+
+        return Answer(content=answer, traceback=traceback)
